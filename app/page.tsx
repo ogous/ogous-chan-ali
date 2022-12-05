0d82d7b1a2e2322bd0e78d2c03183ca6a8221518
oguzhanali <oguzxan.ali@gmail.com>
2022-12-05 14:37:04 +0300

    update style

--- app/page.tsx
+++ app/page.tsx
@@ -49,14 +49,14 @@ export default function HomePage() {
     }
   ]
   return (
-    <div className="relative flex min-h-screen items-center justify-center">
+    <div className="relative flex min-h-screen items-center justify-center pb-6">
       <Image
         src={HeroImage}
         alt="web3 world"
         className="absolute inset-0 z-[-1] h-full w-full rounded-lg object-cover blur-sm"
         loading="lazy"
       />
-      <div className="flex max-w-screen-xl flex-1 flex-col items-center justify-center">
+      <div className="flex max-w-screen-xl flex-1 flex-col items-center justify-center p-4">
         <Image
           src={OgousImage}
           alt="Ogous Chan Ali"
@@ -64,50 +64,48 @@ export default function HomePage() {
           loading="lazy"
           className="z-10 -mb-4 rounded-full border-4 border-layer border-opacity-20 shadow-lg"
         />
-        <div className="rounded-2xl border-4 border-layer border-opacity-20 bg-layer bg-opacity-10 p-4">
-          <h1 className="mb-6 mt-8 bg-gradient-to-r from-tertiary via-primary to-secondary bg-clip-text text-center font-lato text-6xl font-black leading-[70px] text-transparent">
-            Ogous Chan Ali
-          </h1>
-          <div className="flex flex-col items-center justify-center text-center font-poppins">
-            <span className="mb-8">Thinks about and hacks with:</span>
+        <h1 className="mb-6 mt-8 bg-gradient-to-r from-tertiary via-primary to-secondary bg-clip-text text-center font-lato text-6xl font-black leading-[70px] text-transparent">
+          Ogous Chan Ali
+        </h1>
+        <div className="flex flex-col items-center justify-center text-center font-poppins">
+          <span className="mb-8">Thinks about and hacks with:</span>
 
-            <p className="mb-6 flex flex-col md:flex-row">
-              {tags.map((tag) => (
-                <span
-                  key={tag}
-                  className={
-                    'mb-1 rounded-full bg-gradient-to-r from-tertiary via-primary to-secondary py-1 px-3 font-bold text-layer md:mr-1'
-                  }
-                >
-                  #{tag}
-                </span>
-              ))}
-            </p>
-
-            <p className="inline-block max-w-md text-center">
-              With over ten years experience as a software engineer, team
-              leader, product owner in the tech industry, I am enthusiastically
-              focused on building highly scalable applications and
-              infrastructure solutions for my partners.
-            </p>
-          </div>
-          <br></br>
-          <ul className="flex flex-wrap items-center justify-center">
-            {socials.map((i, idx) => (
-              <li
-                key={i.slug}
-                className={clsx(
-                  idx !== socials.length - 1 &&
-                    'mr-2 after:ml-2 after:content-["|"]'
-                )}
+          <p className="mb-6 flex flex-col md:flex-row">
+            {tags.map((tag) => (
+              <span
+                key={tag}
+                className={
+                  'mb-1 rounded-full bg-gradient-to-r from-tertiary via-primary to-secondary py-1 px-3 font-bold text-layer md:mr-1'
+                }
               >
-                <Link href={i.link} target={'_blank'}>
-                  {i.slug.charAt(0).toUpperCase() + i.slug.slice(1)}
-                </Link>
-              </li>
+                #{tag}
+              </span>
             ))}
-          </ul>
+          </p>
+
+          <p className="inline-block max-w-md text-center">
+            With over ten years experience as a software engineer, team leader,
+            product owner in the tech industry, I am enthusiastically focused on
+            building highly scalable applications and infrastructure solutions
+            for my partners.
+          </p>
         </div>
+        <br></br>
+        <ul className="bottom-0 left-0 mx-auto flex w-full flex-wrap items-center justify-center px-4 pb-4 md:absolute">
+          {socials.map((i, idx) => (
+            <li
+              key={i.slug}
+              className={clsx(
+                idx !== socials.length - 1 &&
+                  'mr-2 after:ml-2 after:content-["|"]'
+              )}
+            >
+              <Link href={i.link} target={'_blank'}>
+                {i.slug.charAt(0).toUpperCase() + i.slug.slice(1)}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
